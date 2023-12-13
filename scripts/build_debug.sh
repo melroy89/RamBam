ebcbf4cab24580777e67ee9e47d3979224a59d63
Melroy van den Berg <melroy@melroy.org>
2023-12-13 01:20:55 +0100

    Support docker clang-format check and fix

--- scripts/build_debug.sh
+++ scripts/build_debug.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # By: Melroy van den Berg
 # Description: Debug build
-
 rm -rf build_debug
 cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -B build_debug
 cmake --build ./build_debug --target rambam --config Debug
