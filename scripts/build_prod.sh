3f03371a92a291c251d9289ceb38599b07a700eb
Melroy van den Berg <melroy@melroy.org>
2024-02-19 02:26:58 +0100

    Add PACKAGE option

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -2,7 +2,7 @@
 # By: Melroy van den Berg
 # Description: Release production build + create Debian/RPM and compressed file.
 rm -rf build_prod
-cmake -GNinja -DCMAKE_INSTALL_PREFIX:PATH=/usr -DCMAKE_BUILD_TYPE=Release -B build_prod
+cmake -GNinja -DCMAKE_INSTALL_PREFIX:PATH=/usr -DPACKAGE=ON -DCMAKE_BUILD_TYPE=Release -B build_prod
 cmake --build ./build_prod --target rambam --config Release
 echo "INFO: Building packages..."
 cd build_prod
