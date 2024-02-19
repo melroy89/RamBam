a6f88cefabb11326ff3ba921c208af9e2c9cfed8
Melroy van den Berg <melroy@melroy.org>
2024-02-19 02:34:14 +0100

    Add source archive script

---
+++ scripts/create_source_archive.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# By: Melroy van den Berg
+# Description: Create custom source code archive with version.txt
+# Depends on one environment variable: $CI_COMMIT_TAG
+
+if [ -z ${CI_COMMIT_TAG} ]; then
+    echo "ERROR: CI_COMMIT_TAG env. variable is not set! Exit"
+    exit 1
+fi
+# Create version.txt
+echo -n "${CI_COMMIT_TAG}" > version.txt
+# Create source archive (tar.gz)
+git archive --format=tar.gz --output=build_prod/RamBam-Source-${APP_VERSION}.tar.gz --add-file=version.txt HEAD
