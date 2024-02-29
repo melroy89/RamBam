568202d4a2f78ce106a22d93f81e634ab61b9e40
Melroy van den Berg <melroy@melroy.org>
2024-02-29 23:33:50 +0100

    Add "v" to the tar.gz

--- scripts/create_source_archive.sh
+++ scripts/create_source_archive.sh
@@ -10,4 +10,4 @@ fi
 # Create version.txt
 echo -n "${CI_COMMIT_TAG}" > version.txt
 # Create source archive (tar.gz)
-git archive --format=tar.gz --output=build_prod/RamBam-Source-${CI_COMMIT_TAG}.tar.gz --add-file=version.txt HEAD
+git archive --format=tar.gz --output=build_prod/RamBam-Source-v${CI_COMMIT_TAG}.tar.gz --add-file=version.txt HEAD
